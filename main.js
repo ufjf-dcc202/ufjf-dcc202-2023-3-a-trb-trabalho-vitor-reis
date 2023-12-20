85407df0915cf582c00badf50d7b1dafbbaa9a49
Vitor Reis <vitordesouzareis@gmail.com>
2023-12-20 19:00:26 -0300

    transitando pela coluna com as setas e posicionando o número

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-// Variável para controlar o jogador atual
 let jogadorAtual = 1;
+let colunaSelecionada = 0;
 
-// Lista dos jogadores
+// Listas dos jogadores
 const listasJogador1 = [
     document.getElementById('player1List1'),
     document.getElementById('player1List2'),
@@ -14,55 +14,112 @@ const listasJogador2 = [
     document.getElementById('player2List3')
 ];
 
-// Variável para controlar se o jogador pode clicar
-let podeClicar = true;
-
-// Função para verificar se é o turno do jogador 1
-function ehTurnoJogador1() {
-    return jogadorAtual === 1;
+// Função para adicionar classe 'selected' à coluna selecionada
+function marcarColunaSelecionada() {
+    listasJogador1[colunaSelecionada].classList.add('selected');
 }
 
-// Função para lidar com o clique em um quadrado
-function handleClique(square) {
-    const rect = square.getBoundingClientRect();
-    const squareTop = rect.top;
+// Função para remover classe 'selected' da coluna anteriormente selecionada
+function desmarcarColunaSelecionada() {
+    listasJogador1[colunaSelecionada].classList.remove('selected');
+}
 
-    // Obtém a altura da janela do navegador
-    const windowHeight = window.innerHeight;
+// Função para verificar se é o turno do jogador 1 e atualizar a interface
+function atualizarTurnoJogador1() {
+    marcarColunaSelecionada();
+    adicionarEventoClique();
+}
 
-    
-    const splitLine = windowHeight * 0.5;
-    // Verifica se é o turno do jogador 1
-    if ((jogadorAtual === 1) && square.textContent === '' && !square.classList.contains('filled') && podeClicar && squareTop < splitLine) {
-        // Atribui o valor do dado ao quadrado clicado
-        square.textContent = document.getElementById(`resultadoDado${jogadorAtual}`).textContent.split(' ')[3];
+function adicionarEventoEnter() {
+    document.addEventListener('keydown', handleEnterKeyPress);
+}
 
-        // Indica que o quadrado foi corrigido
-        square.classList.add('filled');
+function removerEventoEnter() {
+    document.removeEventListener('keydown', handleEnterKeyPress);
+}
 
-        // Troca de jogador
-        trocarJogador();
+function atualizarColunaSelecionada() {
+    desmarcarColunaSelecionada();
+    listasJogador1[colunaSelecionada].classList.add('selected');
+}
+function handleEnterKeyPress(event) {
+    const key = event.key;
+
+    if (key === 'Enter') {
+        tratarSelecao();
+    } else if (key === 'ArrowRight' && colunaSelecionada < 2) {
+        // Move para a próxima coluna
+        desmarcarColunaSelecionada();
+        colunaSelecionada++;
+        marcarColunaSelecionada();
+    } else if (key === 'ArrowLeft' && colunaSelecionada > 0) {
+        // Move para a coluna anterior
+        desmarcarColunaSelecionada();
+        colunaSelecionada--;
+        marcarColunaSelecionada();
     }
 }
 
-// Função para adicionar eventos de clique
+function handleKeyPress(event) {
+    const key = event.key;
+
+    if (key === 'ArrowRight' && colunaSelecionada < 2) {
+        // Move para a próxima coluna
+        desmarcarColunaSelecionada();
+        colunaSelecionada++;
+        marcarColunaSelecionada();
+    } else if (key === 'ArrowLeft' && colunaSelecionada > 0) {
+        // Move para a coluna anterior
+        desmarcarColunaSelecionada();
+        colunaSelecionada--;
+        marcarColunaSelecionada();
+    }
+}
+
+// Função para tratar a seleção quando a tecla Enter é pressionada
+function tratarSelecao() {
+    const valorDado = document.getElementById(`resultadoDado${jogadorAtual}`).textContent.split(' ')[3];
+
+    // Encontra a posição para adicionar o valor do dado na coluna selecionada
+    let posicao = 2;
+    while (posicao >= 0) {
+        const square = listasJogador1[colunaSelecionada].getElementsByTagName('li')[posicao];
+        if (square.textContent === '') {
+            square.textContent = valorDado;
+            square.classList.add('filled');
+            break;
+        }
+        posicao--;
+    }
+
+    // Se todas as posições estiverem ocupadas, exibe um alerta
+    if (posicao < 0) {
+        alert('Escolha outra coluna, todas estão ocupadas!');
+    }
+
+    // Atualiza a coluna selecionada
+    desmarcarColunaSelecionada();
+
+    // Troca de jogador
+    trocarJogador();
+}
+
+// Função para adicionar eventos de keydown
 function adicionarEventoClique() {
-    document.querySelectorAll('.player-list li').forEach(square => {
-        square.addEventListener('click', () => handleClique(square));
-    });
+    document.addEventListener('keydown', tratarSelecao);
 }
 
-// Função para remover eventos de clique
+// Função para remover eventos de keydown
 function removerEventoClique() {
-    document.querySelectorAll('.player-list li').forEach(square => {
-        square.removeEventListener('click', () => handleClique(square));
-    });
+    document.removeEventListener('keydown', tratarSelecao);
 }
 
+// Função para verificar se o elemento clicado pertence à pontuação do jogador
 function clicouNoScore(element) {
     return element.closest('.score') !== null;
 }
 
+// Função para atualizar a pontuação do jogador na interface
 function atualizarScoreJogador(jogador) {
     const listaJogador = jogador === 1 ? listasJogador1 : listasJogador2;
     const scoreDiv = document.getElementById(`scoreJogador${jogador}`);
@@ -83,7 +140,7 @@ function atualizarScoreJogador(jogador) {
 // Função para rolar o dado do jogador 1
 function rolarDadoJogador1() {
     if (jogadorAtual === 2) {
-        return; // Impede que o jogador 1 role o dado durante o turno do jogador 2
+        return;
     }
 
     // Lógica para gerar um número aleatório de 1 a 6 (representando o dado)
@@ -94,47 +151,33 @@ function rolarDadoJogador1() {
 
     setTimeout(() => {
         podeClicar = true;
-    }, 1000);   
-
-    adicionarEventoClique();
+        atualizarColunaSelecionada();
+        adicionarEventoEnter();
+    }, 1000);
 
     verificarIgualdadeColunasAmbosJogadores();
-
 }
 
 // Função para trocar de jogador
 function trocarJogador() {
-
-
     jogadorAtual = jogadorAtual === 1 ? 2 : 1;
     atualizarScoreJogador(1);
     console.log(jogadorAtual);
 
     // Se o jogador atual for 2, rolar dados automaticamente
     if (jogadorAtual === 2) {
-        
-            // Após rolar o dado, impede que o jogador 1 clique
-            podeClicar = false;
-
-            // Remove os eventos de clique
-            document.querySelectorAll('.player-list li').forEach(square => {
-                square.removeEventListener('click', () => {});
-            });
-            
-            
-            rolarDadoJogador2();
-        
+        podeClicar = false;
+        removerEventoClique();
+        rolarDadoJogador2();
     }
-    
-    
-    }
-
-
+}
 // Função para rolar o dado do jogador 2
 function rolarDadoJogador2() {
     if (jogadorAtual === 1) {
         return;
     }
+
+    removerEventoClique();
 
     const valorDado = Math.floor(Math.random() * 6) + 1;
     const resultadoDadoElement = document.getElementById('resultadoDado2');
@@ -149,38 +192,39 @@ function rolarDadoJogador2() {
             itensLista[j].textContent = valorDado;
             break;
         }
-        
     }
 
     atualizarScoreJogador(2);
 
     console.log(`Rolou dado para Jogador 2. Coluna escolhida: ${colunaJogador2Aleatoria + 1}`);
 
-    verificarIgualdadeColunasAmbosJogadores();
+    verificarIgualdadeColunasAmbosJogadores(colunaJogador2Aleatoria);
 
     trocarJogador();
 }
 
+// Função para verificar igualdade nas colunas de ambos os jogadores
 function verificarIgualdadeColunasAmbosJogadores() {
-    for (let linha = 0; linha < 3; linha++) {
-        for (let coluna = 0; coluna < 3; coluna++) {
-            const elementoJogador1 = listasJogador1[coluna].getElementsByTagName('li')[linha].textContent;
-            const elementoJogador2 = listasJogador2[coluna].getElementsByTagName('li')[linha].textContent;
+    for (let colunaJogador1 = 0; colunaJogador1 < 3; colunaJogador1++) {
+        for (let linha = 0; linha < 3; linha++) {
+            const valorJogador1 = listasJogador1[colunaJogador1].getElementsByTagName('li')[linha].textContent;
 
-            if (elementoJogador1 === elementoJogador2 && elementoJogador1 !== '') {
-                console.log(`Elemento igual encontrado na linha ${linha + 1}, posição ${coluna + 1} para ambos os jogadores!`);
-                
+            // Verifica se o valor existe na coluna do jogador 2
+            for (let colunaJogador2 = 0; colunaJogador2 < 3; colunaJogador2++) {
+                if (colunaJogador2 !== colunaJogador1) {
+                    const valorJogador2 = listasJogador2[colunaJogador2].getElementsByTagName('li')[linha].textContent;
+
+                    if (valorJogador1 === valorJogador2 && valorJogador1 !== '') {
+                        console.log(`Elemento igual encontrado na linha ${linha + 1}, posição ${colunaJogador1 + 1} para ambos os jogadores!`);
+
+                        // Limpa o elemento na coluna do jogador 2
+                        listasJogador2[colunaJogador2].getElementsByTagName('li')[linha].textContent = '';
+                        listasJogador2[colunaJogador2].getElementsByTagName('li')[linha].classList.remove('filled');
+                    }
+                }
             }
         }
     }
 }
 
-
-
-
-
-
-
-
-
-
+document.getElementById('btnRolarDado').addEventListener('click', rolarDadoJogador1);
